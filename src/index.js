b097e5510be5b46ee52e0d20195a4dc9ce63523d
odinokun <odinokun@gmail.com>
2018-11-28 15:54:52 +0200

    did repositories list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // инклюдим js файлы
 import { loadRepository } from './js/test';
 
+import repositoriesFn from './hbs/github-rep.hbs';
+
 // инклюдим scss файл
 import './style.scss';
 
@@ -8,12 +10,19 @@ import './style.scss';
 const ava = document.getElementById('github-header__ava-img');
 // имя юзера
 const userName = document.getElementById('github-header__title-name');
-
-// запускаем функции
-// loadRepository();
+// бщее кол-во репозиториев
+const allRepo = document.getElementById('github-title__title--all');
+// левая колонка
+const leftColumn = document.getElementById('github-body__left');
 
 loadRepository()
-    .then(function (data) {
-        ava.src = data[0].owner.avatar_url;
-        userName.innerText = data[0].owner.login;
+    .then(function (repositoriesArray) {
+        ava.src = repositoriesArray[0].owner.avatar_url;
+        userName.innerText = repositoriesArray[0].owner.login;
+        allRepo.innerText = repositoriesArray.length;
+
+        const repositoriesHTML = repositoriesFn({repositoriesList: repositoriesArray});
+
+        leftColumn.innerHTML = repositoriesHTML;
+
     });
