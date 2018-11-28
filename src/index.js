4cb90e6e054239ef62cee70c8f5c6ee0434181ba
odinokun <odinokun@gmail.com>
2018-11-28 16:18:40 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // инклюдим js файлы
-import { loadRepository } from './js/test';
+import { loadRepository } from './js/load';
 
 import repositoriesFn from './hbs/github-rep.hbs';
 
@@ -10,19 +10,19 @@ import './style.scss';
 const ava = document.getElementById('github-header__ava-img');
 // имя юзера
 const userName = document.getElementById('github-header__title-name');
-// бщее кол-во репозиториев
+// общее кол-во репозиториев
 const allRepo = document.getElementById('github-title__title--all');
 // левая колонка
 const leftColumn = document.getElementById('github-body__left');
 
 loadRepository()
-    .then(function (repositoriesArray) {
+    .then(repositoriesArray => {
+        // объявляем основную аватарку
         ava.src = repositoriesArray[0].owner.avatar_url;
+        // объявляем логин юзера
         userName.innerText = repositoriesArray[0].owner.login;
+        // объявляем общее кол-во репозиториев
         allRepo.innerText = repositoriesArray.length;
-
-        const repositoriesHTML = repositoriesFn({repositoriesList: repositoriesArray});
-
-        leftColumn.innerHTML = repositoriesHTML;
-
+        // заполняем через цикл данные каждого репозитория
+        leftColumn.innerHTML = repositoriesFn({ repositoriesList: repositoriesArray });
     });
