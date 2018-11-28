b0c805d339aa54df621e4ec3bcd3071b3915263f
odinokun <odinokun@gmail.com>
2018-11-28 13:18:40 +0200

    did title name & user avatar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 // инклюдим js файлы
-import { testFunction } from './js/test';
+import { loadRepository } from './js/test';
 
 // инклюдим scss файл
 import './style.scss';
 
+// основная аватарка
+const ava = document.getElementById('github-header__ava-img');
+// имя юзера
+const userName = document.getElementById('github-header__title-name');
+
 // запускаем функции
-testFunction();
+// loadRepository();
+
+loadRepository()
+    .then(function (data) {
+        ava.src = data[0].owner.avatar_url;
+        userName.innerText = data[0].owner.login;
+    });
